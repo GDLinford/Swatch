2b2d6bc74fc4c7c0fcfe879566d767063f4abe28
GDLinford <55755896+GDLinford@users.noreply.github.com>
2019-12-05 13:46:38 +0000

    Swatch tutorial

---
+++ Swatch/Swatch/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Swatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Swatch.SelectedItem += OnSelectedItem;
+        }
+
+        private void OnSelectedItem(int selectedIndex, ImageSource source)
+        {
+            label.Content = "Selected " + selectedIndex;
+            image.Source = source;
+
+            selectedImages.Children.Add(new Image() { Source = source, Stretch = Stretch.None });
+        }
+
+
+        private void Render(object sender, RoutedEventArgs e)
+        {
+            // Poorly grab Width/height
+            int width = (int)selectedImages.ActualWidth;
+            int height = (int)selectedImages.ActualHeight;
+            int dpi = 96;
+
+            //Renders target to a Bitmap (also a component which is visible)
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(width, height, dpi, dpi, PixelFormats.Pbgra32);
+            renderTargetBitmap.Render(selectedImages);
+
+            //Encodes to a PNG
+            PngBitmapEncoder pngImage = new PngBitmapEncoder();
+            pngImage.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+            //write PNG encoded image to file
+            using (System.IO.Stream fileStream = System.IO.File.Create("created.png"))
+            {
+                pngImage.Save(fileStream);
+            }
+
+
+
+        }
+    }
+    
+  
+}
---
+++ Swatch/Swatch/SwatchPanel.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Swatch
+{
+    /// <summary>
+    /// Interaction logic for SwatchPanel.xaml
+    /// </summary>
+    public partial class SwatchPanel : UserControl
+    {
+        //Border Components
+        protected int borderWidth = 2;
+        protected SolidColorBrush selectedBrush = new SolidColorBrush(Colors.Black);
+        protected SolidColorBrush unselectedBrush = new SolidColorBrush(Colors.White);
+
+        //Selected Item
+        protected Border selectedSwatch = null;
+
+        // Events to Fire
+        public delegate void SelectedItemDelegate(int selectedIndex, ImageSource source);
+        public event SelectedItemDelegate SelectedItem;
+
+        // Public properties, but private setters
+        public int SpriteWidth { get; private set; } = 32;
+        public int SpriteHeight { get; private set; } = 32;
+
+
+        public SwatchPanel()
+        {
+            InitializeComponent();
+
+            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+                return;
+
+            //AddSprite("Sprites/dirt.png");
+            //AddSprite("Sprites/dirt2.png");
+            //AddSprite("Sprites/dirt3.png");
+
+            AddTileSheet("Sprites/terrain_atlas.png", SpriteWidth, SpriteHeight);
+        }
+        public void AddSprite(string localFilePath)
+        {
+            var image = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/" + localFilePath)) };
+            image.Stretch = Stretch.None;
+            AddSprite(image);
+        }
+
+        public void AddSprite(Image image)
+        {
+            var border = new Border();
+            border.BorderThickness = new Thickness(borderWidth);
+            border.BorderBrush = unselectedBrush;
+            border.Child = image;
+            border.MouseDown += OnMouseDown;
+
+            stackPanel.Children.Add(border);
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var border = sender as Border;
+            if (selectedSwatch != null)
+                selectedSwatch.BorderBrush = unselectedBrush;
+
+            selectedSwatch = border;
+            border.BorderBrush = selectedBrush;
+            int selectedIndex = stackPanel.Children.IndexOf(border);
+
+            if (selectedSwatch != null)
+            {
+                ImageSource source = (border.Child as Image).Source;
+                SelectedItem?.Invoke(selectedIndex, source);
+            }
+        }
+
+        public void AddTileSheet(string localTileSheetPath, int width, int height)
+        {
+            var imageSource = new BitmapImage(new Uri("pack://application:,,,/" + localTileSheetPath));
+            for (int y = 0; y < imageSource.PixelHeight; y += height)
+            {
+                for (int x = 0; x < imageSource.PixelWidth; x += width)
+                {
+                    var croppedBitmap = new CroppedBitmap(imageSource, new Int32Rect(x, y, width, height));
+                    AddSprite(new Image() { Source = croppedBitmap });
+                }
+            }
+        }
+        
+
+    }
+}
